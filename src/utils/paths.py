b610c971d4c7f659002d30c5c8e2a6d67c579195
João Suzuki <joaosuzuki98@outlook.com>
2025-04-17 23:43:01 -0300

    Add function to retrieve file paths

---
+++ src/utils/paths.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+
+def get_paths(sheets_credential_file):
+    base = Path(__file__).resolve().parent.parent.parent / 'storage'
+    contacts_path = base / 'contacts' / 'contacts.csv'
+    updated_path = base / 'contacts' / 'updated.csv'
+    credentials_path = base / 'credentials' / sheets_credential_file
+    return contacts_path, updated_path, credentials_path
